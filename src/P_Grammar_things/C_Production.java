05d1cca27f2097fc65159ccec6bf5d3652479ee8
devcrack <jaha.devcrack@gmail.com>
2017-04-30 01:17:58 -0500

    Can load the right right of the productions

--- src/P_Grammar_things/C_Production.java
+++ src/P_Grammar_things/C_Production.java
@@ -5,7 +5,7 @@
  */
 package P_Grammar_things;
 
-import P_Symbol.C_Set_Symbol;
+import P_Symbol.C_Symbol;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,21 +15,21 @@ import java.util.regex.Pattern;
  * @author devcrack
  */
 public class C_Production {
-    private ArrayList<C_Set_Symbol>left;
-    private ArrayList<C_Set_Symbol>right;
+    private ArrayList<C_Symbol>left;
+    private ArrayList<C_Symbol>right;
     
     
     public C_Production()
     {
-        this.left = new ArrayList<C_Set_Symbol>();
-        this.right = new ArrayList<C_Set_Symbol>();
+        this.left = new ArrayList<C_Symbol>();
+        this.right = new ArrayList<C_Symbol>();
     }
 
     
     /**
      * @return the left
      */
-    public ArrayList<C_Set_Symbol> getLeft() {
+    public ArrayList<C_Symbol> getLeft() {
         return left;
     }
     
@@ -37,7 +37,7 @@ public class C_Production {
     /**
      * @return the right
      */
-    public ArrayList<C_Set_Symbol> getRight() {
+    public ArrayList<C_Symbol> getRight() {
         return right;
     }
     
@@ -92,18 +92,48 @@ public class C_Production {
     
     public void ld_production(String str)
     {
-        char crrt_symbol;
+        char crrt_symbol = '\0';
         StringBuilder str_blr = new StringBuilder();
-        
+        C_Symbol nw_smbl = new C_Symbol();
+                
         for(int i = 0; i < str.length(); i++)
         {
             
-            crrt_symbol = str.charAt(i);
-           int j;
-            j = 0;
+            crrt_symbol = str.charAt(i);          
             if( (this.is_Metacharecter(crrt_symbol) == true && i > 0 && str.charAt(i - 1) == '\\') || is_valid_symbol(crrt_symbol))
                     str_blr.append(crrt_symbol);
+            switch(crrt_symbol)
+            {
+                case '<':
+                    if(i > 0 && str.charAt(i - 1) != '\\' && str_blr.length() > 0) {
+                            this.insert_TerminalS(str_blr.toString());
+                            str_blr = new StringBuilder();
+                    }
+                    break;
+                case '>':
+                    if(i > 0 && str.charAt(i - 1) != '\\') {
+                        nw_smbl = new C_Symbol();
+                        nw_smbl.set_NT(str_blr.toString());
+                        this.right.add(nw_smbl);
+                        str_blr = new StringBuilder();
+                    }
+                    break;
+                    default:
+                        if(i == (str.length() -1))
+                            this.insert_TerminalS(str_blr.toString());
+            }
         }
+    }
+    
+    private void insert_TerminalS(String str_terminal)
+    {
+        C_Symbol nw_smbl = new C_Symbol();
+        
+         if(str_terminal.charAt(0) == '~')
+             nw_smbl.setEpsilon(str_terminal);
+         else
+             nw_smbl.set_T(str_terminal);
+         this.right.add(nw_smbl);
     }
     
     private boolean is_Metacharecter(char c)
@@ -134,6 +164,14 @@ public class C_Production {
         if( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
             return true;
         return false;
+    }
+    
+    public void set_Left(String str_left)
+    {
+        C_Symbol smbl = new C_Symbol();
+        
+        smbl.set_NT(str_left);
+        this.left.add(smbl);
     }
 }
 
