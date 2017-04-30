bf8be7b15d889e1ed143eeb0585acaab6e168641
devcrack <jaha.devcrack@gmail.com>
2017-04-30 01:27:36 -0500

    Class added for the management of epsilon

---
+++ src/P_Symbol/C_Epsilon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package P_Symbol;
+
+/**
+ *
+ * @author devcrack
+ */
+public class C_Epsilon extends C_Atomic_Symbol{
+    
+    private char epsilon;
+    
+    public C_Epsilon() { super(); }
+    
+    /** Get this symbol basically ~
+     * @return the epsilon
+     */
+    public String getEpsilon() {
+        return super.get_Atomic_Symbol();
+    }
+
+}
